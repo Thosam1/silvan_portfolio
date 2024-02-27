d68d99d700ac829fcd0efa99616f66c538a1ca1f
Thösam <thosamtenzin2002@gmail.com>
2024-02-27 08:38:28 +0100

    First Commit

---
+++ app/PrelineScript.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+// import {HSCopyMarkup as HSStaticMethods} from "preline";
+
+export default function PrelineScript() {
+  const path = usePathname();
+
+  useEffect(() => {
+    import("preline/preline");
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+        HSStaticMethods.autoInit();
+    }, 100);
+  }, [path]);
+
+  return null;
+}
---
+++ constants/index.ts
@@ -0,0 +1,164 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/work', key: 'work', label: 'Work' },
+];
+
+export const FULL_NAME = "Silvan Schürmann"
+export const EMAIL = "example@gmail.com"
+
+export const INSTAGRAM_LINK = "https://www.instagram.com/myartjourney_04?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+
+// Team
+export const BOARD = [
+  {
+    name: 'Maéva Schürmann',
+    title: 'Minister',
+    description: "I am a passionate third year student in medicine. I love reading books but most importantly, I am Thösam's girlfriend !",
+    email: "maeva.schurmann@unil.ch",
+    img_url: '/team/maeva.JPG'
+  },
+  {
+    name: 'Maéva Schürmann',
+    title: 'Minister',
+    description: "I am a passionate third year student in medicine. I love reading books but most importantly, I am Thösam's girlfriend !",
+    email: "maeva.schurmann@unil.ch",
+    img_url: '/team/maeva.JPG'
+  },
+  {
+    name: 'Maéva Schürmann',
+    title: 'Minister',
+    description: "I am a passionate third year student in medicine. I love reading books but most importantly, I am Thösam's girlfriend !",
+    email: "maeva.schurmann@unil.ch",
+    img_url: '/team/maeva.JPG'
+  },
+  {
+    name: 'Maéva Schürmann',
+    title: 'Minister',
+    description: "I am a passionate third year student in medicine. I love reading books but most importantly, I am Thösam's girlfriend !",
+    email: "maeva.schurmann@unil.ch",
+    img_url: '/team/maeva.JPG'
+  },
+  {
+    name: 'Maéva Schürmann',
+    title: 'Minister',
+    description: "I am a passionate third year student in medicine. I love reading books but most importantly, I am Thösam's girlfriend !",
+    email: "maeva.schurmann@unil.ch",
+    img_url: '/team/maeva.JPG'
+  },
+  {
+    name: 'Maéva Schürmann',
+    title: 'Minister',
+    description: "I am a passionate third year student in medicine. I love reading books but most importantly, I am Thösam's girlfriend !",
+    email: "maeva.schurmann@unil.ch",
+    img_url: '/team/maeva.JPG'
+  },
+  {
+    name: 'Maéva Schürmann',
+    title: 'Minister',
+    description: "I am a passionate third year student in medicine. I love reading books but most importantly, I am Thösam's girlfriend !",
+    email: "maeva.schurmann@unil.ch",
+    img_url: '/team/maeva.JPG'
+  },
+]
+
+// FAQ Scrubs Night
+export const FAQ_SCRUBS_NIGHT = [
+  {
+    question: "When do the application window open ?",
+    answer: "You will receive an email with all the necessary information 2 month before the event. So don't forget to check your mail inbox regularly !!! "
+  },
+  {
+    question: "When do the application window open ?",
+    answer: "You will receive an email with all the necessary information 2 month before the event. So don't forget to check your mail inbox regularly !!! "
+  },
+  {
+    question: "When do the application window open ?",
+    answer: "You will receive an email with all the necessary information 2 month before the event. So don't forget to check your mail inbox regularly !!! "
+  },
+  {
+    question: "When do the application window open ?",
+    answer: "You will receive an email with all the necessary information 2 month before the event. So don't forget to check your mail inbox regularly !!! "
+  },
+  {
+    question: "When do the application window open ?",
+    answer: "You will receive an email with all the necessary information 2 month before the event. So don't forget to check your mail inbox regularly !!! "
+  },
+  {
+    question: "When do the application window open ?",
+    answer: "You will receive an email with all the necessary information 2 month before the event. So don't forget to check your mail inbox regularly !!! "
+  }
+]
+
+// FOOTER SECTION
+export const SOCIALS = {
+  links: [
+    {
+      name: "Email",
+      url: "mailto:yolo@gmail.com",
+      svg_path: "/email.svg",
+    },
+    {
+      name: "Instagram",
+      url: "instagram.com",
+      svg_path: "/instagram.svg",
+    },
+    {
+      name: "Youtube",
+      url: "youtube.com",
+      svg_path: "/youtube.svg'",
+    },
+  ],
+};
+
+// Scrubs Night November 2023
+export const SCRUBS_NIGHT_NOVEMBER_2023 = [
+  {
+    img_url: "/events/scrubs_night/november_2023/0.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/1.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/2.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/3.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/4.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/5.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/6.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/7.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/8.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/9.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/10.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/11.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/12.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/13.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/14.JPG",
+  },
+  {
+    img_url: "/events/scrubs_night/november_2023/15.JPG",
+  },
+]
